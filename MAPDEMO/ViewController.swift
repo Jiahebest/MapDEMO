a640094ea4829149e32a7f42d4532d998f7cf5ea
gdcp <gdcp@m25.local>
2018-06-05 11:01:17 +0800

    map

--- MAPDEMO/ViewController.swift
+++ MAPDEMO/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
+import MapKit
+class ViewController: UIViewController,MKMapViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let mapView = MKMapView(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height))
+        mapView.mapType = .standard
+        mapView.region = MKCoordinateRegion(center: CLLocationCoordinate2DMake(23.16,113.23), span: MKCoordinateSpanMake(1,1))
+        let pn = MKPointAnnotation()
+        pn.coordinate = CLLocationCoordinate2DMake(23.16, 113.23)
+        pn.title = "GuangZhou"
+        pn.subtitle = "GuangDong,China"
+        mapView.addAnnotation(pn)
+        self.view.addSubview(mapView)
     }
 
+    public func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView?{
+        let view = MKAnnotationView(annotation: annotation, reuseIdentifier:"id")
+        view.canShowCallout = true
+        let view1 = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        view1.backgroundColor = UIColor.blue
+        let view2 = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+         view1.backgroundColor = UIColor.red
+        view.leftCalloutAccessoryView = view1
+        view.rightCalloutAccessoryView = view2
+        view.isDraggable = true
+        return view
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
